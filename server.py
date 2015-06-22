2fc10ed6b1f3662a9b1fc6875d9dc8d6b8c0ec70
James Sully <sullyj3@gmail.com>
2015-06-22 17:17:29 +1000

    make server serve stuff

--- server.py
+++ server.py
@@ -3,9 +3,8 @@ app = Flask(__name__)
 app.debug = False
 
 @app.route('/')
-def hello_world():
-    #assert(False)
-    return 'YOLO'
+def index():
+    return send_from_directory("static/", "index.html")
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80)
